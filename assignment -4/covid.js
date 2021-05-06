85fa0fd7046eac9b6ad73c914e0d59e41b139638
codequee4 <nankundabridget1920@gmail.com>
2021-05-06 13:31:11 +0300

    add covid.js

---
+++ assignment -4/covid.js
@@ -0,0 +1,16 @@
+function temperature(person){
+    var results;
+    var temperature=[];
+    if(temperature == 33){
+        results = 'normal temperature';
+    }
+    else if (temperature >=35){
+    results ='watch your temperature';
+    }
+   else if (temperature >37){
+       results='test for covid 19';
+   }
+   else{
+       result='covid 19 negative';
+   }
+   console.log(results);
